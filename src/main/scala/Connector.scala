7a56d8b9c82ddd83c961374412b65d7bbc2aca8b
Paul Dijou <paul.dijou@gmail.com>
2014-07-19 13:08:39 +1200

    Improve config

--- src/main/scala/Connector.scala
+++ src/main/scala/Connector.scala
@@ -4,14 +4,26 @@ import scala.concurrent.Future
 
 import play.api.libs.json.{Reads, Writes}
 
-import io.yard.models.{Organization, Module, ModuleConfiguration, Provider, ProviderConfiguration}
+import io.yard.common.models.{Organization, Module, ModuleConfiguration, Provider, ProviderConfiguration}
 
 trait Connector {
-  def read[T <: ProviderConfiguration](organization: Organization, provider: Provider)(implicit reader: Reads[T]): Future[Option[T]]
+  def orgs(implicit reader: Reads[Organization]): Seq[Organization]
 
-  def save[T <: ProviderConfiguration](organization: Organization, provider: Provider)(implicit writer: Writes[T]): Future[Boolean]
+  def read[T <: ProviderConfiguration](provider: Provider, organization: Option[Organization])(implicit reader: Reads[T]): Future[Option[T]]
 
-  def read[T <: ModuleConfiguration](organization: Organization, module: Module)(implicit reader: Reads[T]): Future[Option[T]]
+  def save[T <: ProviderConfiguration](provider: Provider, organization: Option[Organization])(implicit writer: Writes[T]): Future[Boolean]
 
-  def save[T <: ModuleConfiguration](organization: Organization, module: Module)(implicit writer: Writes[T]): Future[Boolean]
+  def read[T <: ModuleConfiguration](module: Module, organization: Option[Organization])(implicit reader: Reads[T]): Future[Option[T]]
+
+  def save[T <: ModuleConfiguration](module: Module, organization: Option[Organization])(implicit writer: Writes[T]): Future[Boolean]
+
+  object organizations {
+    import io.yard.common.models.Organization
+
+    def all: Seq[Organization] = orgs
+    def default: Organization = orgs.apply(0)
+    def byNameOption(name: String): Option[Organization] = orgs.find( _.name == name )
+    def byName(name: String): Organization = organizations.byNameOption(name) getOrElse organizations.default
+    def from(value: String): Organization = organizations.byName(value)
+  }
 }
